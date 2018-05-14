0e1a4f433bc396fa056812730cd45fa4cb3c678a
Brandon Green <brandon@brandonleeproductions.com>
2018-05-14 02:30:43 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(move)
+     index = move.to_i - 1 # Declares the index variable, changes it to an integer and -1 to correspond to the array indexes
+     index
+end
+
+def move(board, index, player = "X") # In this instance, we give player a default value of "X"
+    board[index] = player # Updates the board array index location with player, or in this case, the default value "X"
+    
+end
