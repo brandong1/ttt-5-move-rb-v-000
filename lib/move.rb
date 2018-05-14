9ca6c3a8c400913e03a3ebb1615c97cf908679e7
Brandon Green <brandon@brandonleeproductions.com>
2018-05-14 02:32:56 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -2,11 +2,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+board = [" "," "," "," "," "," "," "," "," "]
 end
 
 # code your input_to_index and move method here!
